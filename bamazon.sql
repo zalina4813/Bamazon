745abddf6af5e7d5770843c76446ab1508ca8b84
Zalina Magiday <57268539+zalina4813@users.noreply.github.com>
2020-07-01 18:59:07 -0400

    Homework

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+
+CREATE DATABASE BamazonDB;
+USE Bamazon;
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Dove Shampoo', 'Cosmetics', 5.75, 500),
+		('Dove Conditioner', 'Cosmetics', 6.25, 627),
+		('Glad 12 Gal Trash Bags', 'Grocery', 5.99, 300),
+		('Brawny Paper Towels', 'Grocery', 4.25, 400),
+		('Granny Smith Apples', 'Produce', 0.35, 800),
+		('Chiquita Bannana', 'Produce', 0.20, 10000),
+		('Tropicana Orange Juice', 'Grocery', 4.45, 267),
+		('Horizon Organic Milk', 'Grocery', 4.50, 200),
+		('Huggies Diapers', 'Children', 2.75, 476),
+		('Charmin Toiler Paper', 'Grocery', 12.99, 575),
+		('Pampers Baby Wipes', 'Children', 1.50, 423),
+		('Yoga Mat', 'Sports', 12.75, 150);
---
+++ bamazonCustomer.js
@@ -0,0 +1,115 @@
+var inquirer = require('inquirer');
+var mysql2 = require('mysql2');
+
+var connection = mysql2.createConnection({
+	host: 'localhost',
+	port: 3306,
+	
+	user: 'root',
+
+	password: 'Landrover02!',
+	database: 'BamazonDB'
+});
+
+
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
+
+function promptUserPurchase() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+					
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						connection.end();
+					})
+				} else {
+					console.log('Insufficient quantity!.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+function displayInventory() {
+	
+	queryStr = 'SELECT * FROM products';
+
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].department_name + '  //  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	  	promptUserPurchase();
+	})
+}
+
+function runBamazon() {
+	
+	displayInventory();
+}
+
+runBamazon();
